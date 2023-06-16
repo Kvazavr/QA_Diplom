a57add4646462f66c28bd3411d1cd7ee966df328
Elena Nekrasova <kvazavr@yandex.ru>
2023-06-16 15:02:27 +0400

    Created DataHelper and PaymentPage

--- src/main/java/ru/netology/PaymentPage.java
+++ src/main/java/ru/netology/PaymentPage.java
@@ -9,13 +9,14 @@ import static com.codeborne.selenide.Selenide.$x;
 public class PaymentPage {
     private SelenideElement codeFieldTitle = $x("//h2/[contains(text(), 'Путешествие дня')]");
     private SelenideElement codeFieldTitleOfTour = $x("//h3[contains(text(), 'Мараккэш')]");
-    private SelenideElement buttonToBuy = $("[data-test-id=action-verify]");button class="button__text" Купить
+    private SelenideElement buttonToBuy = $("[data-test-id=action-verify]");button class="button__text"Купить
     private SelenideElement codeFieldTitleOfTour = $x("//*[contains(text(), 'Мараккэш')]");
 
 
     public DashboardPage() {
         codeFieldTitle.shouldBe(Condition.visible);
         codeFieldTitleOfTour.shouldBe(Condition.visible);
+
     }
 
 }
